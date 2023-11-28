333934feef342bb0a6abfefc9e6266b8d3da3576
gaetano-94 <gaetano.panico1994@gmail.com>
2023-11-28 19:32:58 +0100

    feat: todo and newtodo

--- js/script.js
+++ js/script.js
@@ -1,1 +1,40 @@
 'use strict';
+
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      todoObj: [
+        {
+          text: 'Fare la spesa',
+          done: false,
+        },
+        {
+          text: 'Non dire le bugie come Gaetano',
+          done: true,
+        },
+        {
+          text: 'Studiare Vue Js',
+          done: true,
+        },
+        {
+          text: 'Andare su la luna',
+          done: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    removeTodo: function (index) {
+      this.todoObj.splice(index, 1);
+    },
+
+    addNewTodo: function () {
+      if (this.newTodoText.trim() !== '') {
+        this.todoObj.push({ text: this.newTodoText, done: false });
+        this.newTodoText = '';
+      }
+    },
+  },
+}).mount('#app');
